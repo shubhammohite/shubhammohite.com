e30614e72480756a69776666dc3be5f5b197458e
sanjaykrishna1203 <sanjaykrishna1203@gmail.com>
2023-06-30 14:21:51 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
+import { AboutComponent } from './body/about/about.component';
+import { SkillsComponent } from './body/skills/skills.component';
+import { ExperienceComponent } from './body/experience/experience.component';
+import { ProjectsComponent } from './body/projects/projects.component';
+import { ContactComponent } from './body/contact/contact.component';
+import { HomeComponent } from './body/home/home.component';
+import { EducationComponent } from './body/education/education.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent,
+    AboutComponent,
+    SkillsComponent,
+    ExperienceComponent,
+    ProjectsComponent,
+    ContactComponent,
+    HomeComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule,
